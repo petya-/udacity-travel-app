6b5aa990c6700e8a78275aea13d0696f47e6679f
Petya Buchkova <petya.buchkova@gmail.com>
2020-04-25 14:17:58 +0200

    Setup webpack config for development

---
+++ webpack.dev.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/client/index.js",
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    publicPath: "/",
+    filename: "[name].js",
+  },
+  target: "web",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        // Loads the javacript into html template provided.
+        // Entry point is set below in HtmlWebPackPlugin in Plugins
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            //options: { minimize: true }
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+    }),
+  ],
+};

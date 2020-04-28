df67d53a9543448d479f2418e9e3917617bae1c8
Petya Buchkova <petya.buchkova@gmail.com>
2020-04-28 20:24:10 +0200

    Added endpoint tests

--- src/tests/server.spec.js
+++ src/tests/server.spec.js
@@ -1,9 +1,42 @@
 const app = require("../server/server"); // Link to your server file
 const supertest = require("supertest");
 const request = supertest(app);
-
+let trip = {
+  city: "test",
+  country: "test country",
+};
 describe("Test API endpoints", () => {
   test("GET / -> Successfuly render index.html", async () => {
     const response = await request.get("/").expect(200);
+  });
+
+  test("POST /trips -> Successfuly create a trip", async () => {
+    const response = await request
+      .post("/trips")
+      .send({ trip })
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    trip.id = response.body[0].id;
+
+    expect(response.body[0].city).toBe(trip.city);
+    expect(response.body[0].country).toBe(trip.country);
+    expect(response.body[0].id).toBeTruthy();
+  });
+
+  test("GET /trips -> Successfuly get trips", async () => {
+    const response = await request.get("/trips").expect(200);
+    expect(response.body.length).toBe(1);
+  });
+
+  test("Delete /trips -> Successfuly delete a trip", async () => {
+    const response = await request
+      .delete(`/trips/${trip.id}`)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    expect(response.body.length).toBe(0);
   });
 });
